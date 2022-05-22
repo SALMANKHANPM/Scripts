0c702cb20f2ef5098fdcae57bd15a8f529d64967
P.M.SALMAN KHAN <patanmohammadsalmankhan@gmail.com>
2022-05-22 18:06:02 +0530

    scripts : added scripts to choose distro from arch & debian based.
    Signed-off-by: P.M.SALMAN KHAN  <patanmohammadsalmankhan@gmail.com>

--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,21 @@
 #!/usr/bin/env bash
+
+# importing Prerequisite Packages.
+source .ignore/ignore
+
+# Introduction
+printf %"s\n" "Hello World!" "Hope you doing great!" "This script help you to setup your system with the possible environment." | typing
+
+# Asking whether the user want to setup his system to ready for building.
+printf %"s\n" "Want to setup your system for Source Development and Building?" "If you want to ? Type 'yes', else enter to skip." | typing
+
+# Reading userinput.
+read $setup
+
+# Using if and else to setup or skip
+if [ $setup = "yes" ]
+then
+    bash tools/setup.sh
+else
+    printf %"s\n" "Skipping setup!" | typing
+fi
--- tools/setup.sh
+++ tools/setup.sh
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+
+# Reporting which the user choosen
+printf %"s\n" "Since you've choosen to setup." "Which distro you're using?" "Examples?" "Arch Based (ArchLinux, ManjaroLinux, EndeavourOS, etc..)" "Debian Based (Ubuntu, PopOS, ZorinOS, LinuxMint, etc..)" | typing
+
+# Asking about which distro they use?
+printf %"s\n" "" "Which distro in which you want to setup ?" "debian based?" "arch based ?" "if you don't, no worries!" "Please check the below results.." "" | typing && 
+cat /etc/*-release | grep ID_LIKE && printf " " && sleep 4
+
+# Choosing the de to initialize the setup.
+printf %"s\n" "If you see ubuntu or debian?" "Type 'ubuntu' !" "If you see arch?" "Type 'arch' !" | typing
+
+# Reading userinput.
+read -r $distro
+
+# Using Conditions to run setup for the userinput.
+if [ "$distro" = "arch" ]
+then
+    printf %"s\n" "You've chosen Arch.." "RUNNING SCRIPTS" "" | typing && bash arch/env_arch.sh
+else
+    printf %"s\n" "You've chosen Ubuntu.." "RUNNING SCRIPTS" "" | typing && bash debian/env_debian.sh
+fi
+
